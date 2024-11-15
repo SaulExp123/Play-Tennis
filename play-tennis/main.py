cc18a0152b6fc2d656892ad19ba8d89d7a3be3b9
SaulExp123 <71776477+SaulExp123@users.noreply.github.com>
2024-11-15 12:39:26 +0000

    Version final del Tenis

--- play-tennis/main.py
+++ play-tennis/main.py
@@ -1,66 +1,54 @@
 def run(points: str) -> str:
-    SETS_FOR_TIEBREAK = 6
+    GAMES_FOR_TIEBREAK = 6
+    MIN_DIFF_WIN = 2
+    POINTS_FOR_GAME = 4
+    GAMES_FOR_SET = 6
+    GAMES_FOR_WIN_TIEBREAK = 7
+
     result = ''
-    sets_a = 0
-    sets_b = 0
+    games_a = 0
+    games_b = 0
     points_a = 0
     points_b = 0
     tiebreak = False
-    a_win_tiebreak = False
-    b_win_tiebreak = False
 
-    for char in points:
-        if sets_a == SETS_FOR_TIEBREAK and sets_b == SETS_FOR_TIEBREAK:
+    for player in points:
+        if games_a == GAMES_FOR_TIEBREAK and games_b == GAMES_FOR_TIEBREAK:
             tiebreak = True
-        if tiebreak:
-            if char == 'A':
+        match player:
+            case 'A':
                 points_a += 1
-            else:
+            case 'B':
                 points_b += 1
-            if points_a >= 7 and points_a - points_b >= 2:
-                a_win_tiebreak = True
+            case _:
+                continue
+        if not tiebreak:
+            if points_a >= POINTS_FOR_GAME and points_a - points_b >= MIN_DIFF_WIN:
+                games_a += 1
                 points_a = 0
                 points_b = 0
-            elif points_b >= 7 and points_b - points_a >= 2:
-                b_win_tiebreak = True
+            elif points_b >= POINTS_FOR_GAME and points_b - points_a >= MIN_DIFF_WIN:
+                games_b += 1
                 points_a = 0
                 points_b = 0
-
-            if a_win_tiebreak:
-                result += f'{sets_a + 1}-{sets_b} '
-                sets_a = 0
-                sets_b = 0
-                a_win_tiebreak = False
-                tiebreak = False
-            elif b_win_tiebreak:
-                result += f'{sets_a}-{sets_b + 1} '
-                sets_a = 0
-                sets_b = 0
-                b_win_tiebreak = False
-                tiebreak = False
-
+            if (games_a >= GAMES_FOR_SET and games_a - games_b >= MIN_DIFF_WIN) or (
+                games_b >= GAMES_FOR_SET and games_b - games_a >= MIN_DIFF_WIN
+            ):
+                result += f'{games_a}-{games_b} '
+                games_a = 0
+                games_b = 0
         else:
-            if char == 'A':
-                points_a += 1
-            else:
-                points_b += 1
-            if points_a >= 4 and points_a - points_b >= 2:
-                sets_a += 1
+            if points_a >= GAMES_FOR_WIN_TIEBREAK and points_a - points_b >= MIN_DIFF_WIN:
+                games_a += 1
+            elif points_b >= GAMES_FOR_WIN_TIEBREAK and points_b - points_a >= MIN_DIFF_WIN:
+                games_b += 1
+            if games_a == GAMES_FOR_WIN_TIEBREAK or games_b == GAMES_FOR_WIN_TIEBREAK:
+                result += f'{games_a }-{games_b} '
+                games_a = 0
+                games_b = 0
                 points_a = 0
                 points_b = 0
-            elif points_b >= 4 and points_b - points_a >= 2:
-                sets_b += 1
-                points_a = 0
-                points_b = 0
-            if sets_a >= 6 and sets_a - sets_b >= 2:
-                result += f'{sets_a}-{sets_b} '
-                sets_a = 0
-                sets_b = 0
-            elif sets_b >= 6 and sets_b - sets_a >= 2:
-                result += f'{sets_a}-{sets_b} '
-                sets_a = 0
-                sets_b = 0
-
+                tiebreak = False
     return result
 
 
